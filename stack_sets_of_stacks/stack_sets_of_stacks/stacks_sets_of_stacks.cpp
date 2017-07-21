e0b420a776e054a2b5cf8a5c99ce13f9b5d3ef89
claytonjwong <claytonjwong@gmail.com>
2017-07-21 10:56:05 -0700

    infinite constructor loop

--- stack_sets_of_stacks/stack_sets_of_stacks/stacks_sets_of_stacks.cpp
+++ stack_sets_of_stacks/stack_sets_of_stacks/stacks_sets_of_stacks.cpp
@@ -34,15 +34,22 @@ class NodeList{
 public:
     NodeList(T val);
 
-    Node<T>* head;
-    NodeList<T>* next;
+    Node<T>* head_node;
+    int node_cnt;
+    
+    NodeList<T>* next_node_list;
 };
 
 
 template <class T>
 NodeList<T>::NodeList(T val){
-    head = new Node<T>(val);
-    next = NULL;
+    
+    // set head node to new node
+    head_node = new Node<T>(val);
+    node_cnt = 1;
+    
+    // set next_list to the next node list
+    next_node_list = NULL;
 }
 
 
@@ -58,7 +65,11 @@ class EmptyStackSetException: public exception
 template <class T>
 class StackSet {
 public:
+    
+    static const int DEFAULT_CAPACITY = 2;
+    
     StackSet(void);
+    StackSet(int capacity);
 
     
     void push(T val){
@@ -68,14 +79,27 @@ public:
             if ( stack_list == NULL ){
             
                 stack_list = new NodeList<T>(val);
-                stack_list->head->next = NULL;
-            
+                
             
             } else {
             
-                Node<T>* new_node = new Node<T>(val);
-                new_node->next = stack_list->head;
-                stack_list->head = new_node;
+                
+                if ( stack_list->node_cnt == stack_capacity ){  // if at capcacity,
+                                                                // add val onto new stack list
+                    NodeList<T>* temp = stack_list;
+                    stack_list = new NodeList<T>(val);
+                    stack_list->next_node_list = temp;
+                    
+                } else { // add new node onto the existing stack list
+                
+                    Node<T>* new_node = new Node<T>(val);
+                    new_node->next = stack_list->head_node;
+                    stack_list->head_node = new_node;
+                    stack_list->node_cnt++;
+                }
+                
+
+
             }
             
         } catch(std::bad_alloc& exc) {
@@ -87,15 +111,22 @@ public:
     
     T pop(){
         
-        if ( stack_list && stack_list->head ){
+        if ( stack_list && stack_list->head_node ){
 
             // pop head off the stack
-            Node<T>* popped_node = stack_list->head;
-            stack_list->head = stack_list->head->next;
+            Node<T>* popped_node = stack_list->head_node;
+            stack_list->head_node = stack_list->head_node->next;
+            stack_list->node_cnt--;
             
             // free the popped head
             T return_value = popped_node->val;
             delete popped_node;
+            
+            // check for empty stack list, if so, try to point towards next stack list
+            if ( stack_list->head_node == NULL ){
+                stack_list = stack_list->next_node_list;
+            }
+            
             
             return return_value;
 
@@ -121,27 +152,51 @@ public:
     }
     
     bool isEmpty(){
-        return stack_list->head == NULL;
+        return stack_list == NULL;
     }
 
 private:
     
     NodeList<T>* stack_list;
+    int stack_capacity;
 };
 
 template <class T>
 StackSet<T>::StackSet(void){
+    StackSet(DEFAULT_CAPACITY);
+}
 
+template <class T>
+StackSet<T>::StackSet(int capacity){
+    stack_list = NULL;
+    stack_capacity = capacity;
 }
 
 int main(int argc, const char * argv[]) {
     
     int val;
-    StackSet<int>* ss = new StackSet<int>;
+    //StackSet<int>* ss = new StackSet<int>(StackSet<int>::DEFAULT_CAPACITY);
+    StackSet<int>* ss = new StackSet<int>();
     
+    ss->push(0);
+    ss->pop();
     ss->push(0);
     ss->push(1);
     ss->push(2);
+    ss->push(3);
+    ss->push(4);
+    ss->push(5);
+    ss->pop();
+    ss->push(5);
+    ss->push(6);
+    ss->push(7);
+    ss->push(8);
+    ss->pop();
+    ss->pop();
+    ss->pop();
+    ss->push(6);
+    ss->push(7);
+    ss->push(8);
     
     while ( ! ss->isEmpty() ){
     
