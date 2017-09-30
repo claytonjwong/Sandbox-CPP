b35ba8f7ae0bbc74f2317b7440a1d3dc4674b4b4
claytonjwong <claytonjwong@gmail.com>
2017-09-29 19:56:17 -0700

    20. Valid Parentheses (more concise)

--- valid_parens/valid_parens/valid_parens.cpp
+++ valid_parens/valid_parens/valid_parens.cpp
@@ -13,60 +13,26 @@
 
 
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
-        
-        vector<char> stack;
-        
-        for (auto curr : s) {
-            
-            //
-            // when an open container is found,
-            // push the corresponding close container
-            // onto the stack
-            //
-            if (curr=='(') {
-                stack.push_back(')');
-                
-            } else if (curr=='{') {
-                stack.push_back('}');
-                
-            } else if (curr=='[') {
-                stack.push_back(']');
-            
-            } else if (stack.size() > 0) {
-                
-                //
-                // this character should be a close container
-                // which should match the previously pushed character
-                //
-                char prev = stack.back();
-                
-                if (curr != prev ) {
-                    return false;
-                } else {
-                    stack.pop_back();
-                }
-                
-            } else /* stack.size() == 0 */ {
-                return false;
-                
+        stack<char> stack{};
+        for (const auto& c : s){
+            switch(c){
+                case '(': stack.push(')'); break;
+                case '{': stack.push('}'); break;
+                case '[': stack.push(']'); break;
+                default:
+                    if (stack.empty() || c!=stack.top()) { return false; }
+                    stack.pop();
+                    break;
             }
         }
-        
-        //
-        // stack size should be 0 if all pushed values were popped and matched
-        //
-        if ( stack.size()==0 ){
-            return true;
-        } else {
-            return false;
-        }
+        return stack.empty() ? true : false;
     }
 };
 
