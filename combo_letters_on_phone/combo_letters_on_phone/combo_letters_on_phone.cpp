ce6a278333fa02e059d2e981514e3e84a138a962
claytonjwong <claytonjwong@gmail.com>
2017-09-29 17:30:33 -0700

    17. Letter Combinations of a Phone Number (more concise)

--- combo_letters_on_phone/combo_letters_on_phone/combo_letters_on_phone.cpp
+++ combo_letters_on_phone/combo_letters_on_phone/combo_letters_on_phone.cpp
@@ -23,49 +23,50 @@
 
 using namespace std;
 
-class Solution {
+class Solution{
 public:
     vector<string> letterCombinations(string digits) {
-        
-        vector<string> combos;
-        
-        unordered_map<char, string> map;
-        map['2'] = "abc";
-        map['3'] = "def";
-        map['4'] = "ghi";
-        map['5'] = "jkl";
-        map['6'] = "mno";
-        map['7'] = "pqrs";
-        map['8'] = "tuv";
-        map['9'] = "wxyz";
-        
-        for ( char digit : digits ){
-            
-            if (map.find(digit) == map.end()) { continue; } // digits w/o letters such as 0 or 1
-            
-            if (combos.empty()){ // first digit, append digit's letters onto the vector of combos
-                
-                for ( char letter : map[digit] ){
-                    
-                    combos.push_back( string(1, letter) );
-                                     
+        vector<string> res{};
+        helper(digits,res);
+        return res;
+    }
+    
+private:
+    void helper(string s, vector<string>& res){
+        while (!s.empty()){
+            string letters=to_string(s.back());
+            s.pop_back();
+            if (res.empty()){
+                for (auto& letter : letters){
+                    res.push_back(string(1,letter));
                 }
-             } else { // create a new vector by appending subsequent digit's letters
-                      // onto the old vector of combos
-                 
-                 vector<string> new_combos;
-                 for (int i=0; i < combos.size(); i++){
-                     for ( char letter : map[digit] ){
-                         new_combos.push_back(combos[i] + string(1, letter) );
-                     }
-                 }
-                 combos = new_combos;
-             }
-         }
-        
-        return combos;
-     }
- };
+            } else {
+                vector<string> prev=move(res), curr{};
+                for (auto& p : prev){
+                    for (auto& letter : letters){
+                        curr.push_back(string(1,letter)+p);
+                    }
+                }
+                res=move(curr);
+            }
+        }
+    }
+    
+    string to_string(const char& c){
+        switch(c){
+            case '2': return "abc";
+            case '3': return "def";
+            case '4': return "ghi";
+            case '5': return "jkl";
+            case '6': return "mno";
+            case '7': return "pqrs";
+            case '8': return "tuv";
+            case '9': return "wxyz";
+            default:
+                return "";
+        }
+    }
+};
 
 
 int main(int argc, const char * argv[]) {
