37ca438ab7981624f507b4ef7fda7c122c695c13
claytonjwong <claytonjwong@gmail.com>
2018-03-09 12:05:12 -0800

    204. Count Primes (fix loop-end)

--- count_primes/count_primes/count_primes.cpp
+++ count_primes/count_primes/count_primes.cpp
@@ -23,7 +23,7 @@ public:
         int cnt=0;
         vector<bool> nums(n,true);
         nums[0]=nums[1]=false;
-        for (int i=2; i<n; ++i)
+        for (int i=2; i<=n; ++i)
             if (nums[i]){
                 ++cnt;
                 for (int j=2; i*j<n; ++j)
