f55e2f4100e1f3132e1210f4283dbe5b133c61e9
claytonjwong <claytonjwong@gmail.com>
2018-03-10 08:15:21 -0800

    151. Reverse Words in a String

--- str_reverse_words/str_reverse_words/str_reverse_words.cpp
+++ str_reverse_words/str_reverse_words/str_reverse_words.cpp
@@ -1,5 +1,14 @@
 /*
  
+ 151. Reverse Words in a String
+ 
+ https://leetcode.com/problems/reverse-words-in-a-string/description/
+ 
+ Given an input string, reverse the string word by word.
+ 
+ For example,
+ Given s = "the sky is blue",
+ return "blue is sky the".
  
  */
 
