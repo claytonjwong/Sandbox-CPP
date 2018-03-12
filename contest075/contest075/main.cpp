cdd35f435632293eeea5f2cfac2def013e4cbdcc
claytonjwong <claytonjwong@gmail.com>
2018-03-12 12:16:32 -0700

    797. All Paths From Source to Target

--- contest075/contest075/main.cpp
+++ contest075/contest075/main.cpp
@@ -33,6 +33,17 @@ public:
     }
 };
  */
+
+/*
+ //AC
+class Solution {
+public:
+    bool rotateString(string A, string B) {
+        return A.size()==B.size() && (A+A).find(B) != string::npos;
+    }
+};
+ */
+
 /*
  //AC
 class Solution {
@@ -49,7 +60,6 @@ private:
             unordered_set<int>& v, vector<int>& p){
         v.insert(i);
         p.push_back(i);
-        
         if (i==t){
             r.push_back(p);
         } else {
@@ -57,48 +67,27 @@ private:
                 if (!v.count(nei))
                     go(r,g,nei,t,v,p);
         }
-        
         v.erase(i);
         p.pop_back();
     }
 };
 */
 
-/*
+// AC
 class Solution {
 public:
-    int bestRotation(vector<int>& A) {
-        return 0;
-    }
-};
- */
-
-// Wrong Answer
-class Solution {
-public:
-    double champagneTower(int poured, int query_row, int query_glass) {
-        double res=0.0;
-        int row=0;
-        while (poured>row+1){
-            ++row;
-            poured-=row;
-        }
-        if (query_row<row) return 1.0;
-        if (query_row>row) return 0.0;
-        vector<vector<double>> ratio(query_row+1,vector<double>(query_row+1));
-        ratio[0][0]=1.0;
-        int size=2;
-        for (int i=1; i<=query_row; ++i){
-            for (int j=0; j<size; ++j){
-                if (j>0){
-                    ratio[i][j]=(ratio[i-1][j-1]+ratio[i-1][j])/2.0;
-                } else {
-                    ratio[i][j]=(ratio[i-1][j])/2.0;
-                }
+    double champagneTower(int K, int M, int N) {
+        vector<vector<double>> glass(M+1,vector<double>(M+1));
+        glass[0][0]=K;
+        for (int i=0; i<M; ++i)
+            for (int j=0; j<=i && j<=N; ++j){
+                if (glass[i][j]<=1) continue; // no spill
+                auto half=(glass[i][j]-1)/2;
+                glass[i+1][j]+=half;
+                glass[i+1][j+1]+=half;
             }
-            ++size;
-        }
-        return poured*ratio[query_row][query_glass];
+                
+        return min(1.0,glass[M][N]);
     }
 };
 
@@ -107,7 +96,7 @@ int main(int argc, const char * argv[]) {
     Solution s;
     
     /*
-    string A="abcde",B="abced";
+    string A="abcde",B="deabc";
     cout << s.rotateString(A, B) << endl;
     */
     
@@ -123,7 +112,8 @@ int main(int argc, const char * argv[]) {
     
     //double champagneTower(int poured, int query_row, int query_glass) {
     
-    cout << s.champagneTower(6, 2, 1) << endl;
+    cout << s.champagneTower(8, 3, 2) << endl;
+    //cout << s.champagneTower2(6, 2, 0) << endl;
     
     
     return 0;
