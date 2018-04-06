3cb49b7f04f3c4a9fedd230ed1752088edb99137
claytonjwong <claytonjwong@gmail.com>
2018-04-06 16:34:41 -0700

    ToH add unit tests

---
+++ next_permutation/next_permutation/next_permutation.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  next_permutation
+//
+//  Created by CLAYTON WONG on 4/6/18.
+//  Copyright © 2018 Clayton Wong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
