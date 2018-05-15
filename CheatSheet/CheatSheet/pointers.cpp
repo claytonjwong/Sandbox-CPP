b39dafec9959ad5a940afb138114304922048bff
claytonjwong <claytonjwong@gmail.com>
2018-05-14 20:00:42 -0700

    cheatsheet pointers

--- CheatSheet/CheatSheet/pointers.cpp
+++ CheatSheet/CheatSheet/pointers.cpp
@@ -1,15 +1,23 @@
 //
-//  main.cpp
+//  pointers.cpp
 //  CheatSheet
 //
 //  Created by CLAYTON WONG on 5/13/18.
 //  Copyright © 2018 Clayton Wong. All rights reserved.
 //
 
-#include <iostream>
+#include <memory>
+
+using namespace std;
+
+template<typename T>
+void sink(unique_ptr<T> releaseMe) {}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    unique_ptr<int> up{new int(13)};
+    int* p=up.get();    // p points towards the address owned by up
+    sink(move(up));     // sink releases the memory pointed towards by up
+
     return 0;
 }
