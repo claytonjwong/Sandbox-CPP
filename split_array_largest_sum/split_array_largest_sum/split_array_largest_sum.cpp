9fe3a37e7ec3474e2026ec7189e5c0fa94b0fdc5
claytonjwong <claytonjwong@gmail.com>
2018-05-30 12:36:28 -0700

    410. Split Array Largest Sum ( incorrect with breadcrumbs )

--- split_array_largest_sum/split_array_largest_sum/split_array_largest_sum.cpp
+++ split_array_largest_sum/split_array_largest_sum/split_array_largest_sum.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
@@ -12,25 +13,31 @@ public:
     int splitArray(vector<int>& A, int K, int ans=INT_MAX) {
         vector<int> S(A.size()+1,0);
         for (int i=1,N=(int)S.size(); i<N; ++i) S[i]=S[i-1]+A[i-1];
-        go(ans,S,K);
+        unordered_set<int> V;
+        go(ans,S,K,V);
         return ans;
     }
 private:
-    int go(int& ans, const vector<int>& S, int K, int beg=0, int sum=0, int maxi=INT_MIN){
-        if (K==0) return 0;
+    void go(int& ans, const vector<int>& S, int K, unordered_set<int>& V,
+            int beg=0, int sum=0, int maxi=INT_MAX){
+        if (K==0) return;
         for (int i=1,N=(int)S.size(); beg+i<=N-K; ++i){
             sum=S[beg+i]-S[beg];
-            maxi=max(sum,go(ans,S,K-1,beg+i));
+            V.insert(sum);
+            go(ans,S,K-1,V,beg+i);
+        }
+        if (!V.empty()){
+            maxi=*max_element(V.begin(), V.end());
+            V.clear();
         }
         ans=min(ans,maxi);
-        return maxi;
     }
 };
 
 int main(int argc, const char * argv[]) {
     
-    vector<int> A{7,2,5,10,8};
-    int K=2;
+    //vector<int> A{10,5,13,4,8,4,5,11,14,9,16,10,20,8}; int K=8;
+    vector<int> A{7,2,5,10,8}; int K=2;
     Solution s;
     auto r=s.splitArray(A,K);
     
