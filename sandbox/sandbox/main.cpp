96cdc6b1415c0374a7b1fe6058e60f4637d0181a
claytonjwong <claytonjwong@gmail.com>
2018-06-01 11:59:45 -0700

    UDUB CPP2 EWeek4

--- sandbox/sandbox/main.cpp
+++ sandbox/sandbox/main.cpp
@@ -1,73 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <string>
+
 using namespace std;
 
-class Base
-{
-public:
-    Base ()
-    :mBaseVal1(100), mBaseVal2(100) {}
-    
-    virtual void Print( ostream & os );
-protected:
-    int        mBaseVal1;
-    int        mBaseVal2;
-};
-
-class Derived : public Base
-{
-public:
-    Derived() : mDerivedVal1(200), mDerivedVal2(200){}
-    
-    virtual void Print( ostream & os );
-    
-private:
-    int        mDerivedVal1;
-    int        mDerivedVal2;
-};
-
-void Base::Print( ostream & os )
-{
-    os << "mBaseVal1 " << mBaseVal1 << " mBaseVal2 " << mBaseVal2 << endl;
+int f(int N){
+    int a=0; for (int c=0,b=1; N--; c=b,b=a) a=b+c; return a;
 }
 
-
-void Derived::Print( ostream & os )
-{
-    Base::Print( os );
-    os << "mDerivedVal1 " << mDerivedVal1 << " mDerivedVal2 " << mDerivedVal2 << endl;
-}
-
-void ProcessObject( Base * bPtr )
-{
-    bPtr->Print( std::cout );
-}
-
-void Foo( Base * bPtr, int numElements )
-{
-    int count = 0;
-    
-    while ( count < numElements )
-    {
-        ProcessObject( bPtr );
-        bPtr ++;
-        ++ count;
-    }
-}
-
-void BaseArrayDerivedArray()
-{
-    Base        bArray[ 2 ];
-    Derived        dArray[ 2 ];
-    
-    Foo( bArray, sizeof(bArray)/sizeof(Base));
-    
-    Foo( dArray, sizeof(dArray)/sizeof(Derived));
-    
+int gcd(int a, int b){
+    if (b==0)
+        return a;
+    return gcd(b,a%b);
 }
 
 int main(){
     
-    BaseArrayDerivedArray();
+    while(true){
+        int a,b;
+        cout << "a,b: "; cin >> a >> b;
+        cout << gcd(a,b) << endl;
+    }
     
     return 0;
 }
