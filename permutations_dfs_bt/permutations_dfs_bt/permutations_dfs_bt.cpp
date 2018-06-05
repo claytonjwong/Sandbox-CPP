458364da7164891197ddd9878e449e8bd72c3cf2
claytonjwong <claytonjwong@gmail.com>
2018-06-05 13:49:26 -0700

    46. Permutations

--- permutations_dfs_bt/permutations_dfs_bt/permutations_dfs_bt.cpp
+++ permutations_dfs_bt/permutations_dfs_bt/permutations_dfs_bt.cpp
@@ -5,6 +5,12 @@
  Q: https://leetcode.com/problems/permutations/description/
  A: https://leetcode.com/problems/permutations/discuss/136424/Straightforward-C++-DFS-with-backtracking
  
+ 
+ 47. Permutations II
+ 
+ Q: https://leetcode.com/problems/permutations-ii/description/
+ A: https://leetcode.com/problems/permutations-ii/discuss/136432/Straightforward-C++-DFS-with-Backtracking
+ 
  */
 
 #include <iostream>
@@ -13,6 +19,7 @@
 
 using namespace std;
 
+/*
 class Solution {
     unordered_set<int> V;
     vector<int> path;
@@ -39,12 +46,49 @@ public:
         return ans;
     }
 };
+*/
+
+class Solution {
+    struct hash{
+        size_t operator()(const vector<int>& A) const {
+            size_t ans=0;
+            for (int i=0,N=(int)A.size(); i<N; ++i)
+                ans+=A[i]*(i+1);
+            return ans;
+        }
+    };
+    unordered_set<vector<int>,hash> ans;
+    unordered_set<int> V;
+    vector<int> path;
+    void go(const vector<int>& A){
+        int M=(int)path.size(),N=(int)A.size();
+        if (M==N){
+            ans.insert(path);
+            return;
+        }
+        for (int i=0; i<N; ++i){
+            if (V.insert(i).second){
+                path.push_back(A[i]);
+                go(A);
+                V.erase(i);
+                path.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& A){
+        ans.clear();
+        go(A);
+        return {ans.begin(),ans.end()};
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     
     Solution s;
     vector<int> A{1,1,2};
-    auto r=s.permute(A);
+    auto r=s.permuteUnique(A);
     
     
     return 0;
