9b5ad55053e5fb8435e1a4ad45e628769ad0bd67
claytonjwong <claytonjwong@gmail.com>
2018-07-20 14:27:31 -0700

    UDUB CPP3 EWeek1 (25/25 TC pass)

---
+++ _UDUB/CPP_3/EWeek1/EWeek1/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "TestHarness.h"
+#include "VectorGraphicStreamer.hpp"
+
+using namespace std;
+using namespace VG;
+
+int main(int argc, char *argv[])
+{
+
+	// random number generator used in some tests
+	srand(::time_t(NULL));
+
+    TestResult tr;
+    TestRegistry::runAllTests(tr);
+
+	// force console screen to hold
+	char ch;
+	std::cin >> ch;
+
+/*
+    if (argc!=3){
+        cerr << "invalid params.  expected infile and outfile." << endl;
+        return -1;
+    }
+    stringstream ss;
+    string infile(argv[1]),outfile(argv[2]);
+    {
+        ifstream fin(infile);
+        if (fin){
+            ss << fin.rdbuf();
+            auto vg=VectorGraphicStreamer::fromXml(ss);
+        }
+    }
+    {
+        ofstream fout(outfile);
+        if (fout){
+            fout << ss.rdbuf();
+        }
+    }
+*/
+    return 0;
+}
