f690546550ea39f7ce5c1e957a1827f5b77ba05b
claytonjwong <claytonjwong@gmail.com>
2018-07-22 20:30:47 -0700

    CLRS CH02 (whitespace)

--- _CLRS/CH02/CH02/sort.hpp
+++ _CLRS/CH02/CH02/sort.hpp
@@ -71,13 +71,13 @@ void insertion_sort2(vector<int>& A) {
             
             A[i+1]=A[i];    // shift the contents of A[i:j] to the right by one
             --i;            // ( this is performed one index i at a time )
-            // until an index i is found to insert the key after
+                            // until an index i is found to insert the key after
             
         }
         
         A[i+1]=key; // insert after A[i]
-        // for case 1) i==-1, A[i+1]==A[0], and A[0] is the smallest value in A[0:j)
-        // for case 2) A[i]<=key, A[i+1]==the location in A to insert the key just after A[i]
+                    // for case 1) i==-1, A[i+1]==A[0], and A[0] is the smallest value in A[0:j)
+                    // for case 2) A[i]<=key, A[i+1]==the location in A to insert the key just after A[i]
         
     }
 }
