6b04eeecfedbf3e0603bf62fba429acdfea49051
claytonjwong <claytonjwong@gmail.com>
2018-07-30 11:10:23 -0700

    use pair<int,int> and hash for unordered_map key

--- tic_tac_toe/tic_tac_toe/tic_tac_toe.hpp
+++ tic_tac_toe/tic_tac_toe/tic_tac_toe.hpp
@@ -6,20 +6,22 @@
 
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace TicTacToe{
     
     enum class Player{X,O};
 
+    
     class Model {
     public:
         
-        Model(int N, int K) : mySize{N},myWinLen{K} {}
+        Model(int N, int K) : mySize{std::min(N,getMaxSize())},myWinLen{K} {}
         
         bool makeMove(int i, int j, Player player){
             if (i<0||i>=mySize||j<0||j>=mySize)
                 return false;
-            auto key=hash(i,j);
+            auto key=std::make_pair(i,j);
             if (myBoard.find(key)==myBoard.end()){
                 myBoard[key]=player;
                 return true;
@@ -29,7 +31,7 @@ namespace TicTacToe{
         }
         
         bool isWin(int i, int j) const {
-            auto key=hash(i,j); auto curr_pos=myBoard.find(key);
+            auto key=std::make_pair(i,j); auto curr_pos=myBoard.find(key);
             if (curr_pos==myBoard.end()) return false;
             //
             // try to find player's myWinLen
@@ -40,7 +42,7 @@ namespace TicTacToe{
                 //
                 // horizontal
                 //
-                auto col_key=hash(i,k); auto curr_col=myBoard.find(col_key);
+                auto col_key=std::make_pair(i,k); auto curr_col=myBoard.find(col_key);
                 if (curr_col!=myBoard.end() && curr_col->second==player){
                     if (++col_cnt==myWinLen) return true;
                 }else{
@@ -49,7 +51,7 @@ namespace TicTacToe{
                 //
                 // vertical
                 //
-                auto row_key=hash(k,j); auto curr_row=myBoard.find(row_key);
+                auto row_key=std::make_pair(k,j); auto curr_row=myBoard.find(row_key);
                 if (curr_row!=myBoard.end() && curr_row->second==player){
                     if (++row_cnt==myWinLen) return true;
                 }else{
@@ -61,7 +63,7 @@ namespace TicTacToe{
                 //
                 // bottom-up diagonal
                 //
-                auto bottom_up_key=hash(i-k,j+k); auto curr_bottom_up=myBoard.find(bottom_up_key);
+                auto bottom_up_key=std::make_pair(i-k,j+k); auto curr_bottom_up=myBoard.find(bottom_up_key);
                 if (curr_bottom_up!=myBoard.end() && curr_bottom_up->second==player){
                     if (++bottom_up_cnt==myWinLen) return true;
                 }else{
@@ -70,7 +72,7 @@ namespace TicTacToe{
                 //
                 // top-down diagonal
                 //
-                auto top_down_key=hash(i+k,j+k); auto curr_top_down=myBoard.find(top_down_key);
+                auto top_down_key=std::make_pair(i+k,j+k); auto curr_top_down=myBoard.find(top_down_key);
                 if (curr_top_down!=myBoard.end() && curr_top_down->second==player){
                     if (++top_down_cnt==myWinLen) return true;
                 }else{
@@ -84,17 +86,21 @@ namespace TicTacToe{
             return false;
         }
         
+        int getMaxSize() const { return MAX_SIZE; }
+        
         int getSize() const { return mySize; }
         
         int getWinLen() const { return myWinLen; }
         
     private:
+        static constexpr int MAX_SIZE=100000;
         int mySize;
         int myWinLen;
-        std::unordered_map<std::string,Player> myBoard;
-        
-        std::string hash(int i, int j) const {
-            return std::to_string(i).append(",").append(std::to_string(j));
-        }
+        struct myHash{
+            std::size_t operator()(const std::pair<int,int>& key) const {
+                return ( key.first * MAX_SIZE+1 ) + key.second;
+            }
+        };
+        std::unordered_map<std::pair<int,int>,Player,myHash> myBoard;
     };
 }
