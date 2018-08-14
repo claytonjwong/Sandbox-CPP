f08cbeaefa639e63bcf9611f0410e2e777ffb15c
claytonjwong <claytonjwong@gmail.com>
2018-08-14 16:22:19 -0700

    UDUB CPP download documentation

---
+++ _UDUB/CPP_3/EWeek5/EWeek5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EWeek5
+//
+//  Created by CLAYTON WONG on 8/14/18.
+//  Copyright © 2018 Clayton Wong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
