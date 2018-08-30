341b9520fe2049cee786904d8de86c74e8d8110a
Clayton Wong <clayton.wong@ookla.com>
2018-08-29 17:54:43 -0700

    CLion: 890. Find and Replace Pattern

---
+++ find_and_replace_pattern/find_and_replace_pattern.cpp
@@ -0,0 +1,73 @@
+/*
+
+890. Find and Replace Pattern
+
+Q: https://leetcode.com/problems/find-and-replace-pattern/description/
+A: https://leetcode.com/problems/find-and-replace-pattern/discuss/164762/Straightforward-C++
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+using VS=vector<string>;
+class Solution {
+public:
+
+    VS findAndReplacePattern( VS& words, string pattern, VS answer={} )
+    {
+        for ( const auto& word: words )
+        {
+            if (  isMatch( word, pattern )
+              &&  isMatch( pattern, word )  )
+            {
+                answer.push_back( word );
+            }
+        }
+
+        return answer;
+    }
+
+private:
+
+    bool isMatch( const string& word, const string& pattern )
+    {
+        if ( word.size() != pattern.size() )
+        {
+            return false;
+        }
+
+        unordered_map<char,char> umap;
+        bool match = true;
+        for ( auto index = 0; index < word.size(); ++index )
+        {
+            auto it = umap.find( pattern[ index ] );
+            if ( it == umap.end() )
+            {
+                umap[ pattern[ index ] ] = word[ index ];
+            }
+            else
+            {
+                if ( it->second != word[ index ] )
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+int main() {
+
+    Solution s;
+    VS words{ "abc","deq","mee","aqq","dkd","ccc" }; string pattern{ "abb" };
+    auto res = s.findAndReplacePattern( words, pattern );
+
+    return 0;
+}
