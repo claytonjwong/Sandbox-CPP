4432a98f89f31d0628cf639095d2129e0eb7f38c
Clayton Wong <clayton.wong@ookla.com>
2018-09-09 09:20:58 -0700

    UDUB CPP3 EWeek5 additional XML use case

--- _UDUB/CPP_3/EWeek5/Unit_Test/VectorGraphicStreamerTest.cpp
+++ _UDUB/CPP_3/EWeek5/Unit_Test/VectorGraphicStreamerTest.cpp
@@ -13,25 +13,23 @@ using namespace std;
 using namespace VG;
 
 
-// how to test file system without touching it?  we can only test till stringstream / string?
+TEST(fromFileExists,VectorGraphicStreamer){
+    VectorGraphic vg=VectorGraphicStreamer::getVectorGraphicFromFile("inout.xml");
+    CHECK(vg.getPointCount()==4);
+}
 
-//TEST(fromFileExists,VectorGraphicStreamer){
-//    VectorGraphic vg=VectorGraphicStreamer::getVectorGraphicFromFile("inout.xml");
-//    CHECK(vg.getPointCount()==4);
-//}
-//
-//TEST(toFileDefault,VectorGraphicStreamer){
-//    string filename="default.xml";
-//    VectorGraphic vg1,vg2;
-//    VectorGraphicStreamer::setVectorGraphicToFile(vg1, filename);
-//    VectorGraphicStreamer::getVectorGraphicFromFile(filename);
-//    CHECK(vg1==vg2);
-//}
-//
-//TEST(toFileInXml,VectorGraphicStreamer){
-//    string filename="in.xml";
-//    auto vg1 = VectorGraphicStreamer::getVectorGraphicFromFile(filename);
-//    VectorGraphicStreamer::setVectorGraphicToFile(vg1, filename);
-//    auto vg2 = VectorGraphicStreamer::getVectorGraphicFromFile(filename);
-//    CHECK(vg1==vg2);
-//}
+TEST(toFileDefault,VectorGraphicStreamer){
+    string filename="default.xml";
+    VectorGraphic vg1,vg2;
+    VectorGraphicStreamer::setVectorGraphicToFile(vg1, filename);
+    VectorGraphicStreamer::getVectorGraphicFromFile(filename);
+    CHECK(vg1==vg2);
+}
+
+TEST(toFileInXml,VectorGraphicStreamer){
+    string filename="in.xml";
+    auto vg1 = VectorGraphicStreamer::getVectorGraphicFromFile(filename);
+    VectorGraphicStreamer::setVectorGraphicToFile(vg1, filename);
+    auto vg2 = VectorGraphicStreamer::getVectorGraphicFromFile(filename);
+    CHECK(vg1==vg2);
+}
