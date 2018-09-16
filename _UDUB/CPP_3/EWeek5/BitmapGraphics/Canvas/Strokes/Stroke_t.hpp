7360310d0e9c6aafdce5a432b709939fc089a32c
Clayton Wong <clayton.wong@ookla.com>
2018-09-16 14:43:04 -0700

    UDUB CPP3 EWeek5 separate interface/implementation for Stroke_t

--- _UDUB/CPP_3/EWeek5/BitmapGraphics/Canvas/Strokes/Stroke_t.hpp
+++ _UDUB/CPP_3/EWeek5/BitmapGraphics/Canvas/Strokes/Stroke_t.hpp
@@ -32,42 +32,6 @@ namespace BitmapGraphics
         Color myColor;
     };
     
-    
-    template< typename PenType >
-    Stroke_t< PenType >::Stroke_t ( const std::string& name, const size_t size, const Color& color ) :
-    myName{ name },
-    mySize{ size },
-    myColor{ color }
-    {
-    }
-    
-    
-    template< typename PenType >
-    const std::string& Stroke_t< PenType >::getName ( ) const noexcept
-    {
-        return myName;
-    }
-    
-    
-    template< typename PenType >
-    size_t Stroke_t< PenType >::getSize ( ) const noexcept
-    {
-        return mySize;
-    }
-    
-    
-    template< typename PenType >
-    Color Stroke_t< PenType >::getColor ( ) const noexcept
-    {
-        return myColor;
-    }
-    
-    
-    template< typename PenType >
-    HPen Stroke_t< PenType >::createPen ( ) const noexcept
-    {
-        return std::make_shared< PenType >( myName, mySize, myColor );
-    }
-
-    
 } // namespace BitmapGraphics
+
+#include "Stroke_t_impl.hpp"
