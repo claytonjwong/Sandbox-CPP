f5a72d9942310330b8a3d53b3a4c212c8feebb1e
claytonjwong <claytonjwong@gmail.com>
2018-10-06 21:26:24 -0700

    various UDUB stuff

---
+++ contest105/contest105/contest105.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  contest105
+//
+//  Created by CLAYTON WONG on 10/6/18.
+//  Copyright © 2018 Clayton Wong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
